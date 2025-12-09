7f40b52b6c7315056d7c2527e69821e6e445694f
wyf9 <wyf9@wyf9.top>
2025-12-10 06:37:32 +0800

    rm submodule

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-def main():
-    print("Hello from landing!")
+# coding: utf-8
+
+import logging
+from contextvars import ContextVar
+
+from loguru import logger as l
+from fastapi import FastAPI
+
+from 
+
+VERSION = '2025.12.10'
+reqid: ContextVar[str] = ContextVar('landing_reqid', default='not-in-request')
+
+app = FastAPI(
+    docs_url=None,
+    redoc_url=None,
+    openapi_url=None
+)
 
 
-if __name__ == "__main__":
-    main()
